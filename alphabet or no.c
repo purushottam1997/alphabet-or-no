52391a2bfa70622d3e979f39d7bbd1282aee3c32
purushottam1997 <45572099+purushottam1997@users.noreply.github.com>
2019-06-11 15:52:05 +0530

    Create alphabet or no.c

---
+++ alphabet or no.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a character: ");
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("%c Alphabet",c);
+    else
+        printf("%c No",c);
+    return 0;
+}
